a9c8105869b579f0393c341d0beaf3ac649c4bfc
marcosgilmar <marcosgilmarvalentini@gmail.com>
2026-03-04 11:48:21 -0300

    refactor: separate header by client/server components

---
+++ src/app/(board)/header/search-input.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { Input } from "@/components/input";
+import { SearchIcon } from "lucide-react";
+import { debounce, parseAsString, useQueryState } from "nuqs";
+import { ChangeEvent } from "react";
+
+export function SearchInput() {
+  const [search, setSearch] = useQueryState("q", parseAsString.withDefault(""));
+
+  function handleSearchUpdate(event: ChangeEvent<HTMLInputElement>) {
+    setSearch(event.target.value, {
+      limitUrlUpdates: event.target.value !== "" ? debounce(500) : undefined,
+    });
+  }
+  return (
+    <div className="relative">
+      <SearchIcon className="absolute size-4 text-navy-200 left-3.5 top-1/2 -translate-y-1/2" />
+      <Input value={search} onChange={handleSearchUpdate} className="pl-10" />
+    </div>
+  );
+}
