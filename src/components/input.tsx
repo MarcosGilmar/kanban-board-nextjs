3000934c10bf4651a6d27aa6204d042e2b704eca
marcosgilmar <marcosgilmarvalentini@gmail.com>
2026-03-04 11:27:33 -0300

    feat: implement authentication using Better Auth

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -6,11 +6,12 @@ type InputProps = ComponentProps<"input">;
 export function Input({ className, ...props }: InputProps) {
   return (
     <input
+    placeholder="Search for features..."
       className={twMerge(
         `bg-navy-900 border-[0.5px] border-navy-500 h-10 
-        placeholder-navy-200 pl-3 pr-3 rounded-lg text-sm text-navy-50
-        outline-none focus-visible:ring-2 focus-visible:ring-navy-400 
-        focus-visible:ring-offset-2 focus-visible:ring-offset-navy-950`,
+          placeholder-navy-200 pl-3 pr-3 rounded-lg text-sm text-navy-50
+          outline-none focus-visible:ring-2 focus-visible:ring-navy-400 
+          focus-visible:ring-offset-2 focus-visible:ring-offset-navy-950`,
         className,
       )}
       {...props}
